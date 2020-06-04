915c6adc293b3d0acc20fdded5f7b52a22807ce1
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-06-04 16:46:46 +0300

    Updates

---
+++ precache-manifest.729d5cb74b1958bd852531cf1c2eab58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7df93a3b1bf8ab432401f331c540bec",
+    "url": "/movie-posters/index.html"
+  },
+  {
+    "revision": "c10b85b90a6b50575081",
+    "url": "/movie-posters/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33d71445f60088cea213",
+    "url": "/movie-posters/static/js/2.887bbc79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-posters/static/js/2.887bbc79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10b85b90a6b50575081",
+    "url": "/movie-posters/static/js/main.60bc96fa.chunk.js"
+  },
+  {
+    "revision": "d821473c87d8721848a2",
+    "url": "/movie-posters/static/js/runtime-main.9cdbcfae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-posters/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/movie-posters/precache-manifest.729d5cb74b1958bd852531cf1c2eab58.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-posters/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
